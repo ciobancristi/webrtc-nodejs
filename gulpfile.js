3fb607f00b616db3a92a0ac5dcda0426a1a21bb2
Cris Bitbucket <ciobancristi@gmail.com>
2017-06-21 10:25:17 +0300

    added recordRTC, gulp-clean + task to delete videos in uploads folder

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,15 @@
 
 var _ = require('lodash');
 var gulp = require('gulp');
+var clean = require('gulp-clean');
 
 gulp.task('copy-assets', function () {
     var assets = {
         js: [
             './node_modules/bootstrap/dist/js/bootstrap.min.js',
             './node_modules/jquery/dist/jquery.min.js',
-            './node_modules/rtcmulticonnection-v3/dist/RTCMultiConnection.js'
+            './node_modules/rtcmulticonnection-v3/dist/RTCMultiConnection.js',
+            './node_modules/recordrtc/RecordRTC.min.js'
         ],
         css: ['./node_modules/bootstrap/dist/css/bootstrap.min.css',
             './node_modules/font-awesome/css/font-awesome.min.css'
@@ -17,4 +19,9 @@ gulp.task('copy-assets', function () {
     _(assets).forEach(function (assets, type) {
         gulp.src(assets).pipe(gulp.dest('./public/' + type));
     });
+});
+
+gulp.task('clean-uploads', function () {
+    return gulp.src('uploads/*.webm', { read: false })
+        .pipe(clean());
 });
