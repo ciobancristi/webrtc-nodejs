b81fd3b0903027a6856144d865c03e8f31c30b95
Cris Bitbucket <ciobancristi@gmail.com>
2017-06-22 11:35:44 +0300

    added AWS and chokidar

--- app.js
+++ app.js
@@ -13,30 +13,33 @@ dotenv.load();
 
 var routes = require('./routes/index');
 var user = require('./routes/user');
+const videoUploader = require('./services/videoUploader');
+
+videoUploader.watch();
 
 // This will configure Passport to use Auth0
-var strategy = new Auth0Strategy({
-  domain: process.env.AUTH0_DOMAIN,
-  clientID: process.env.AUTH0_CLIENT_ID,
-  clientSecret: process.env.AUTH0_CLIENT_SECRET,
-  callbackURL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:5000/callback'
-}, function (accessToken, refreshToken, extraParams, profile, done) {
-  // accessToken is the token to call Auth0 API (not needed in the most cases)
-  // extraParams.id_token has the JSON Web Token
-  // profile has all the information from the user
-  return done(null, profile);
-});
+// var strategy = new Auth0Strategy({
+//   domain: process.env.AUTH0_DOMAIN,
+//   clientID: process.env.AUTH0_CLIENT_ID,
+//   clientSecret: process.env.AUTH0_CLIENT_SECRET,
+//   callbackURL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:5000/callback'
+// }, function (accessToken, refreshToken, extraParams, profile, done) {
+//   // accessToken is the token to call Auth0 API (not needed in the most cases)
+//   // extraParams.id_token has the JSON Web Token
+//   // profile has all the information from the user
+//   return done(null, profile);
+// });
 
-passport.use(strategy);
+//passport.use(strategy);
 
 // you can use this section to keep a smaller payload
-passport.serializeUser(function (user, done) {
-  done(null, user);
-});
+// passport.serializeUser(function (user, done) {
+//   done(null, user);
+// });
 
-passport.deserializeUser(function (user, done) {
-  done(null, user);
-});
+// passport.deserializeUser(function (user, done) {
+//   done(null, user);
+// });
 
 var app = express();
 
@@ -56,8 +59,8 @@ app.use(session({
   saveUninitialized: true
 }));
 
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
